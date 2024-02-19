5b58b4a5e5374eea8331df428b540e49198919ce
myamusashi <jinxprogilang666@gmail.com>
2024-02-19 14:23:20 +0700

    change entire structure

---
+++ src/static/js/script.js
@@ -0,0 +1,30 @@
+window.addEventListener("load", () => {
+  const loader = document.querySelector(".loader");
+
+  loader.classList.add("loader-hidden");
+
+  loader.addEventListener("transitionend", () => {
+    document.body.removeChild("loader");
+  });
+});
+
+function toggleMenu() {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry);
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    } else {
+      entry.target.classList.remove("show");
+    }
+  });
+});
+
+const hiddenElement = document.querySelectorAll(".icon__experience");
+hiddenElement.forEach((el) => observer.observe(el));
